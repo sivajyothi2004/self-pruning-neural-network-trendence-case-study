db5465ceb7fc177bf5a89b17340042ff6c016c72
Sivajyothi B <156826794+sivajyothi2004@users.noreply.github.com>
2026-04-24 11:54:02 +0530

    Add files via upload

--- self_pruning_network.py
+++ self_pruning_network.py
@@ -4,7 +4,7 @@ Self-Pruning Neural Network on CIFAR-10
 A feed-forward network that learns to prune itself during training
 using learnable gate parameters and L1 sparsity regularization.
 
-Author: [Your Name]
+Author: Sivajyothi B
 """
 
 import torch
